91c6f760e34cdb97a7a011643ad01f23f8bdc0e4
“lixianglei” <1728276353@qq.com>
2017-06-10 14:21:14 +0800

    6.10 14:20

--- app/src/main/java/com/example/admin/people_blood/MainActivity.java
+++ app/src/main/java/com/example/admin/people_blood/MainActivity.java
@@ -7,8 +7,8 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
-
 import com.example.admin.people_blood.adapter.MyFragmentAdapter;
 import com.example.admin.people_blood.base.BaseActivity;
 import com.example.admin.people_blood.base.BaseFragment;
@@ -22,18 +22,20 @@ import java.util.List;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-import static android.os.Build.VERSION_CODES.M;
-
 public class MainActivity extends BaseActivity {
+
     @Bind(R.id.Title_Text)
     TextView TitleText;
     @Bind(R.id.Main_ViewPager)
     ViewPager MainViewPager;
     @Bind(R.id.Main_Tablayout)
     TabLayout MainTablayout;
-  private List<BaseFragment>  fragments;
-    private List<String>   strings;
-    private MyFragmentAdapter  fragmentAdapter;
+    @Bind(R.id.activity_main)
+    RelativeLayout activityMain;
+    private List<BaseFragment> fragments;
+    private List<String> strings;
+    private MyFragmentAdapter fragmentAdapter;
+
     @Override
     protected int layoutId() {
         return R.layout.activity_main;
@@ -42,8 +44,8 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void initView() {
         TitleText.setText("医生在线");
-       fragments=new ArrayList<>();
-        strings=new ArrayList<>();
+        fragments = new ArrayList<>();
+        strings = new ArrayList<>();
         fragments.add(new DoctorLineFragment());
         fragments.add(new BloodManagerFragment());
         fragments.add(new PersonCenterFragment());
@@ -51,25 +53,27 @@ public class MainActivity extends BaseActivity {
         strings.add("血压管理");
         strings.add("个人中心");
         MainTablayout.setTabMode(TabLayout.MODE_FIXED);
-       fragmentAdapter=new MyFragmentAdapter(getSupportFragmentManager(),fragments,strings);
+        fragmentAdapter = new MyFragmentAdapter(getSupportFragmentManager(), fragments, strings);
         MainViewPager.setAdapter(fragmentAdapter);
         //Tablayout自定义view绑定ViewPager
         MainViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(MainTablayout));
         MainTablayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(MainViewPager));
         MainTablayout.setupWithViewPager(MainViewPager);
-        MainTablayout.getTabAt(0).setCustomView(tab_icon(strings.get(0),R.drawable.doctorline));
-        MainTablayout.getTabAt(1).setCustomView(tab_icon(strings.get(1),R.drawable.booldmanager));
-        MainTablayout.getTabAt(2).setCustomView(tab_icon(strings.get(2),R.drawable.personcenter));
+        MainTablayout.getTabAt(0).setCustomView(tab_icon(strings.get(0), R.drawable.doctorline));
+        MainTablayout.getTabAt(1).setCustomView(tab_icon(strings.get(1), R.drawable.booldmanager));
+        MainTablayout.getTabAt(2).setCustomView(tab_icon(strings.get(2), R.drawable.personcenter));
 
     }
-    private View tab_icon(String name, int iconID){
-        View newtab =  LayoutInflater.from(this).inflate(R.layout.tablayout_item,null);
+
+    private View tab_icon(String name, int iconID) {
+        View newtab = LayoutInflater.from(this).inflate(R.layout.tablayout_item, null);
         TextView tv = (TextView) newtab.findViewById(R.id.tablayout_text);
         tv.setText(name);
-        ImageView im = (ImageView)newtab.findViewById(R.id.tablayout_image);
+        ImageView im = (ImageView) newtab.findViewById(R.id.tablayout_image);
         im.setImageResource(iconID);
         return newtab;
     }
+
     @Override
     protected void loadData() {
 
@@ -80,4 +84,10 @@ public class MainActivity extends BaseActivity {
 
     }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
+    }
 }
--- app/src/main/java/com/example/admin/people_blood/view/fragment/PersonCenterFragment.java
+++ app/src/main/java/com/example/admin/people_blood/view/fragment/PersonCenterFragment.java
@@ -1,7 +1,25 @@
 package com.example.admin.people_blood.view.fragment;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import com.example.admin.people_blood.R;
 import com.example.admin.people_blood.base.BaseFragment;
+import com.example.admin.people_blood.view.fragment.activity.Activity_Message;
+import com.example.admin.people_blood.view.fragment.activity.Activity_My_JiaHao;
+import com.example.admin.people_blood.view.fragment.activity.Activity_My_ShouCang;
+import com.example.admin.people_blood.view.fragment.activity.Activity_PerSonMessage;
+import com.example.admin.people_blood.view.fragment.activity.Activity_SheZhi;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * 项目名称: 血压测量
@@ -14,9 +32,26 @@ import com.example.admin.people_blood.base.BaseFragment;
  */
 
 public class PersonCenterFragment extends BaseFragment {
+
+
+    @Bind(R.id.Login_Image)
+    TextView LoginImage;
+    @Bind(R.id.imageView)
+    ImageView imageView;
+    @Bind(R.id.My_JiaHao)
+    LinearLayout MyJiaHao;
+    @Bind(R.id.My_ShouCang)
+    LinearLayout MyShouCang;
+    @Bind(R.id.My_ZiLiao)
+    LinearLayout MyZiLiao;
+    @Bind(R.id.My_XiaoXi)
+    LinearLayout MyXiaoXi;
+    @Bind(R.id.My_SheZhi)
+    LinearLayout MySheZhi;
+
     @Override
     protected int ViewID() {
-        return R.layout.fragment_item;
+        return R.layout.person_center;
     }
 
     @Override
@@ -32,5 +67,49 @@ public class PersonCenterFragment extends BaseFragment {
     @Override
     protected void listener() {
 
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO: inflate a fragment view
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+
+    @OnClick({R.id.Login_Image, R.id.imageView, R.id.My_JiaHao, R.id.My_ShouCang, R.id.My_ZiLiao, R.id.My_XiaoXi, R.id.My_SheZhi})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.Login_Image:
+                break;
+            case R.id.imageView:
+                break;
+            case R.id.My_JiaHao:
+                Intent intent=new Intent(getContext(), Activity_My_JiaHao.class);
+                startActivity(intent);
+                break;
+            case R.id.My_ShouCang:
+                Intent intent1=new Intent(getContext(), Activity_My_ShouCang.class);
+                startActivity(intent1);
+                break;
+            case R.id.My_ZiLiao:
+                Intent intent2=new Intent(getContext(), Activity_PerSonMessage.class);
+                startActivity(intent2);
+                break;
+            case R.id.My_XiaoXi:
+                Intent intent3=new Intent(getContext(), Activity_Message.class);
+                startActivity(intent3);
+                break;
+            case R.id.My_SheZhi:
+                Intent intent4=new Intent(getContext(), Activity_SheZhi.class);
+                startActivity(intent4);
+                break;
+        }
     }
 }
