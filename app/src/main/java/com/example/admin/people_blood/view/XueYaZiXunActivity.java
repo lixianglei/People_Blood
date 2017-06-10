c772492dec0528122137d843b36be9f325b21ad4
Location_醉梦 <920847827@qq.com>
2017-06-10 17:49:35 +0800

    修改了网络接口 并且增加了屏幕适配

---
+++ app/src/main/java/com/example/admin/people_blood/view/XueYaZiXunActivity.java
@@ -0,0 +1,36 @@
+package com.example.admin.people_blood.view;
+
+import com.example.admin.people_blood.R;
+import com.example.admin.people_blood.base.BaseActivity;
+
+/**
+ * 项目名称: 血压宝
+ * 类描述:
+ * 创建人: 田晓龙
+ * 创建时间: 2017/6/10 17:44
+ * 修改人:
+ * 修改内容:
+ * 修改时间:
+ */
+
+public class XueYaZiXunActivity extends BaseActivity {
+    @Override
+    protected int layoutId() {
+        return R.layout.activity_xueyazixun;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+
+    @Override
+    protected void listener() {
+
+    }
+}
