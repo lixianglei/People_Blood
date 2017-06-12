fe6187e507c86355324edcf116761fac00d8fb41
“lixianglei” <1728276353@qq.com>
2017-06-12 20:03:04 +0800

    lixianglei  6.12  20:01(2)

--- app/src/main/java/com/example/admin/people_blood/view/activity/Activity_MessageFanKui.java
+++ app/src/main/java/com/example/admin/people_blood/view/activity/Activity_MessageFanKui.java
@@ -17,6 +17,8 @@ import butterknife.OnClick;
  */
 
 public class Activity_MessageFanKui extends BaseActivity {
+
+
     @Bind(R.id.left_image)
     ImageView leftImage;
     @Bind(R.id.left_layout)
